8e6f398a062bbc3a8dd1bb074a17c455feec1012
JosephSchechter <fuzzyslipers75+github@gmail.com>
2013-08-01 13:22:16 -0400

    added some math
    Signed-off-by: JosephSchechter <fuzzyslipers75+github@gmail.com>

--- src/Hello.java
+++ src/Hello.java
@@ -8,6 +8,10 @@ public class Hello {
 		// TODO Auto-generated method stub
 		System.out.println("hello github");
 		System.out.println("string 2");
+		
+		int x = 40;
+		int y = 7;
+		System.out.println((char)(x+y));
 	}
 
 }
